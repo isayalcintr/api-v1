7e581e603d8ed472fd6243d30e054e39989224f6
isayalcintr <isayalcintr@gmail.com>
2025-03-05 04:35:27 +0300

    Api DOCS with Scramble

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Auth\AuthController;
+use App\Http\Controllers\Api\v1\Task\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,9 @@ Route::prefix('auth')->group(function () {
     });
 })->name('auth.');
 
+Route::middleware('auth:api')->group(function () {
+    Route::apiResource('task', TaskController::class);
+});
 
 
 
